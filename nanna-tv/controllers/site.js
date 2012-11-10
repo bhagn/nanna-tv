38b830e15e68475fd8de98e4a505ba4e060395de
Bhagavan Nagaraj <bhagavan1@gmail.com>
2012-11-10 23:50:24 +0530

    search interface, bootstrap CSS support

--- nanna-tv/controllers/site.js
+++ nanna-tv/controllers/site.js
@@ -1,8 +1,16 @@
 exports.index = function(req, res, next) {
+  var seriesModel = req.app.models.Series;
   global.require(["dojox/dtl/_base", "dojox/dtl/Context", "dojo/node!fs"], function(dtl, Context, fs) {
-    var templateString = fs.readFileSync("views/index.html", 'utf8');
-    var template = new dtl.Template(templateString);
-    var context = new Context({name: 'Alak-Bulak'});
-    res.send(template.render(context));
+    seriesModel.find(function(err, series) {
+      if(err)
+        res.send(err.message + "<br/>" + err.stack);
+      
+      var templateString = fs.readFileSync("views/index.html", 'utf8');
+      if(series.length == 0)
+        templateString = fs.readFileSync("views/search.html", 'utf8');
+      var template = new dtl.Template(templateString);
+      var context = new Context({series: series});
+      res.send(template.render(context));
+    });
   });
 };
