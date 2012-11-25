41e1454494be343f97550f70fc5af69300ae9843
Bhagavan Nagaraj <bhagavan1@gmail.com>
2012-11-25 13:04:30 +0530

    removed unwanted files

--- nanna-tv/test.js
+++
@@ -1,8 +0,0 @@
-var jsdom = require('jsdom');
-
-jsdom.env('http://www.reuters.com', function(err, window){
-
-                var post = window.document.getElementsByClassName('topStory')[0].textContent;
-
-                console.log(post);
-});
